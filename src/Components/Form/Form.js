9ad7c7842b095fc204b3d0332d1db838adf92329
SuGii <sugii.noob@gmail.com>
2023-06-03 14:54:21 +0200

    ajout de Add-To-Do et delete To-Do

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,40 +1,61 @@
-import {useState} from "react"
+import { useState } from "react"
 import "./Form.css"
 import Item from "../Item/Item"
-import {v4 as uuidv4} from "uuid"
+import { v4 as uuidv4 } from "uuid"
 
 
-function Form(){
+function Form() {
 
     const [dataState, setDataState] = useState([
-        {txt: "Faire le ménage", id: uuidv4()},
-        {txt: "Sortir la poubelle", id: uuidv4() },
-        {txt: "Sortir le chien", id: uuidv4() },
-        {txt: "Aller au supermarché", id:uuidv4() }     
+        { txt: "Faire le ménage", id: uuidv4() },
+        { txt: "Sortir la poubelle", id: uuidv4() },
+        { txt: "Sortir le chien", id: uuidv4() },
+        { txt: "Aller au supermarché", id: uuidv4() }
     ])
 
-    const [stateInput, setStateInput] = useState()
+    const [stateInput, setStateInput] = useState("")
 
     const deleteElement = (id) => {
         const indexOfItem = dataState.findIndex((item) => item.id === id)
-       
+
         const newArrDeleted = [...dataState]
         newArrDeleted.splice(indexOfItem, 1)
-        setDataState(newArrDeleted)       
+        setDataState(newArrDeleted)
     }
 
-    
+    const inputRecup = (e) => {
+        setStateInput(e)
+    }
 
-    return(
-        <>
-            <form>
-                <input type="text" placeholder="Quoi rajouter ?" onInput={stateInput}/>
-                <button>go go go !!!</button>
-            </form>
-            {dataState.map((item) => {
-                return <Item txt={item.txt} key={item.id} id={item.id} deleteFunc={deleteElement}/>
-            })}
-        </>
+    const newTodoAdd = (e) => {
+        e.preventDefault()
+
+        if (stateInput !== "") {
+            const newArrTodo = [...dataState]
+            const newTodo = {
+                txt: stateInput,
+                id: uuidv4()
+            }
+
+            newArrTodo.push(newTodo)
+
+            setDataState(newArrTodo)
+            setStateInput("")
+        }
+    }
+
+    return (
+        <div className="container text-center">
+            <div className="col-lg-6">
+                <form onSubmit={(e) => newTodoAdd(e)}>
+                    <input className="form-control m-5" type="text" value={stateInput} placeholder="Quoi rajouter ?" onInput={(e) => inputRecup(e.target.value)} />
+                    <button className="btn btn-primary">go go go !!!</button>
+                </form>
+                {dataState.map((item) => {
+                    return <Item txt={item.txt} key={item.id} id={item.id} deleteFunc={deleteElement} />
+                })}
+            </div>
+        </div>
     )
 }
 
