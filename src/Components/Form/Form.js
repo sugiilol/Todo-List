852f5cacf519653fc755895cc57b596986767c72
SuGii <sugii.noob@gmail.com>
2023-06-07 07:00:21 +0200

    Add toggle Loading Or CatPicture

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,8 +1,9 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { v4 as uuidv4 } from "uuid"
 import "./Form.css"
 import Item from "../Item/Item"
-import { v4 as uuidv4 } from "uuid"
-
+import CatPicture from "../CatPicture/CatPicture"
+import LoadingPicture from "../LoadingPicture/LoadingPicture"
 
 function Form() {
 
@@ -15,10 +16,11 @@ function Form() {
 
     const [stateInput, setStateInput] = useState("")
 
-    const [testState, setTestState] = useState(1)
+    const [catPictureState, setCatPictureState] = useState()
 
+    const [toggleLoading, setToggleLoading] = useState(true)
 
-    /*******************/
+    /*Func add and delete to-do element*/
 
     const deleteElement = (id) => {
         const indexOfItem = dataState.findIndex((item) => item.id === id)
@@ -49,20 +51,43 @@ function Form() {
         }
     }
 
-    return (
-        <>
+    /* catpicture request */
+
+    useEffect(() => {
+        fetch('https://api.thecatapi.com/v1/images/search')
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                setTimeout(() => {
+                    setCatPictureState(data[0].url)
+                    setToggleLoading(false)
+                }, 2000)
+            })
+    }, [])
+
+    if (toggleLoading) {
+        return (
             <div className="container text-center">
-                <div className="col-lg-6">
-                    <form onSubmit={(e) => newTodoAdd(e)}>
-                        <input className="form-control m-5" type="text" value={stateInput} placeholder="Quoi rajouter ?" onInput={(e) => inputRecup(e.target.value)} />
-                        <button className="btn btn-primary">go go go !!!</button>
-                    </form>
-                    {dataState.map((item) => {
-                        return <Item txt={item.txt} key={item.id} id={item.id} deleteFunc={deleteElement} />
-                    })}
-                </div>
+                <LoadingPicture />
             </div>
-        </>
+        )
+    }
+    return (
+        <div className="container text-center">
+            <div className="col-lg-6">
+                <form onSubmit={(e) => newTodoAdd(e)}>
+                    <input className="form-control m-5" type="text" value={stateInput} placeholder="Quoi rajouter ?" onInput={(e) => inputRecup(e.target.value)} />
+                    <button className="btn btn-primary">go go go !!!</button>
+                </form>
+                {dataState.map((item) => {
+                    return <Item txt={item.txt} key={item.id} id={item.id} deleteFunc={deleteElement} />
+                })}
+            </div>
+            <div>
+                <CatPicture img={catPictureState} />
+            </div>
+        </div>
     )
 }
 
